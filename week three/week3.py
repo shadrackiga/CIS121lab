628b8136fdea9e9083d24fd95aa2cbb89598338c
shadrackiga <shadrackiga6@gmail.com>
2025-09-16 10:43:46 -0500

    a

--- week three/week3.py
+++ week three/week3.py
@@ -1,2 +1,13 @@
 # create by Shadrack Iga
-print("Hello")
+#2. In a) If boolean is not A code will not go to the next step it will automatically move to the next if unlike b) it has to first prove that the first statement is true for it to move to the next steps
+
+light_color =(input('Enter the light color'))
+if (light_color =="red"):
+    print('stop')
+elif (light_color == "yellow"):
+    print ('yield')
+elif (light_color == "green"):
+    print('go')
+else:
+    print("Error")
+
